c3a26bb83c916d9ff8637460658de23a4680c8db
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2025-03-11 21:27:40 +0700

    edit

--- erp/erp/src/main/java/com/project/erp/services/hrm/impl/DepartmentServiceImpl.java
+++ erp/erp/src/main/java/com/project/erp/services/hrm/impl/DepartmentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.project.erp.services.hrm.impl;
 
 import com.project.erp.dto.hrm.departments.DepartmentDTO;
+import com.project.erp.mapper.hrm.DepartmentMapper;
 import com.project.erp.mapper.hrm.EmployeeMapper;
 import com.project.erp.repositories.hrm.DepartmentRepository;
 import com.project.erp.services.hrm.DepartmentService;
@@ -10,18 +11,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
     private final DepartmentRepository departmentRepository;
-    private final EmployeeMapper employeeMapper;
+    private final DepartmentMapper departmentMapper;
 
     @Autowired
     public DepartmentServiceImpl(DepartmentRepository departmentRepository,
-            EmployeeMapper employeeMapper) {
+            DepartmentMapper departmentMapper) {
         this.departmentRepository = departmentRepository;
-        this.employeeMapper = employeeMapper;
+        this.departmentMapper = departmentMapper;
     }
 
     @Override
     public DepartmentDTO getById(Integer id) {
-        return employeeMapper.toDepartmentDTO(departmentRepository.findById(id)
+        return departmentMapper.toDepartmentDTO(departmentRepository.findById(id)
                 .orElseThrow());
     }
 }
